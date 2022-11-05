29aed6b22507be9f689d7ac642f9bf14dc5ad207
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2022-11-06 01:01:43 +0900

    3

--- js_10/js/background.js
+++ js_10/js/background.js
@@ -41,6 +41,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 // const bgImage = document.createElement("img");
 // bgImage.src = `img/${chosenImage}`;
 // document.body.appendChild(bgImage);
-body.style.backgroundImage = url(`img/${chosenImage}`);
+// body.style.backgroundImage = url(`img/${chosenImage}`);
 
-// body.style.backgroundImage = "red";
+body.style.backgroundImage = url("https://www.msn.com/ko-kr/sports/soccer/%EC%86%90%ED%9D%A5%EB%AF%BC-%EB%B6%80%EC%83%81%EC%97%90-%EB%B6%84%EB%85%B8%ED%95%9C-%EC%BD%98%ED%85%8C-11%EC%9B%94-%EC%9B%94%EB%93%9C%EC%BB%B5-%EA%B0%9C%EC%B5%9C%EB%8A%94-%EB%AF%B8%EC%B9%9C-%EC%A7%93/ar-AA13KFPw?ocid=msedgntp&cvid=770c57dd8f6848b4a010d2cd4d8d8265&fullscreen=true#image=1");
